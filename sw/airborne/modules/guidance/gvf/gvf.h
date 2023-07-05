48215d931153282744a5a4649745bc021adb7b69
alfredoFBW <alfredgo@ucm.es>
2023-07-05 15:44:01 +0200

    GVF Line Array improvement

--- sw/airborne/modules/guidance/gvf/gvf.h
+++ sw/airborne/modules/guidance/gvf/gvf.h
@@ -129,6 +129,7 @@ extern bool gvf_line_wp_heading(uint8_t wp, float heading);
 
 // Array of straight lines
 extern bool gvf_lines_array_wp(uint8_t wp0, uint8_t wp1, uint8_t wp2, uint8_t wp3, uint8_t wp4, uint8_t wp5, uint8_t wp6);
+extern bool gvf_lines_array_wp_v2(uint8_t wp0, uint8_t wp1, uint8_t wp2, uint8_t wp3, uint8_t wp4, uint8_t wp5, uint8_t wp6, float d1, float d2);
 
 // Ellipse
 extern bool gvf_ellipse_wp(uint8_t wp, float a, float b, float alpha);
